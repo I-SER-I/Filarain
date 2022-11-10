a29df576b3753ca9af6bbbf0190aea01298ad78b
Sergei Papikian <I_SER_I@hotmail.com>
2022-11-10 04:18:40 +0300

    Add project files.

---
+++ Filarain/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Filarain;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Filarain/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Filarain;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
